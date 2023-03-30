299f09926f57592dbf0060d3e0284b5b70bd8151
zebarcelos <joseribeiro.barcelos@gmail.com>
2023-03-30 20:46:55 -0300

    prog 1

---
+++ lab1/atividade1.py
@@ -0,0 +1,16 @@
+#Questão 1 
+'''for i in range(747 , 939):
+    print(i)'''
+#Questão 3 
+tipoPet = input("Você gostaria de adotar um cachorro ou um gato? Digite C para cachorro ou G para gato. ")
+while tipoPet != "C" and tipoPet !="G":
+    print('\n [ERRO] Digite C ou G')
+    tipoPet = input("Você gostaria de adotar um cachorro ou um gato? Digite C para cachorro ou G para gato. ")
+#Questão 4
+'''def maiorValor(n):
+    lista = []
+    for i in range(0,n):
+        lista.append(input('Digite um valor: '))
+    print(lista)
+minhalista = maiorValor(2)
+print(minhalista)'''
---
+++ prog1/aula1.py
@@ -0,0 +1,6 @@
+contador = 0
+contador1 = 4
+while contador <10 or contador1 <5:
+    print(f'O VALOR É {contador} e {contador1}')
+    contador +=1
+    contador1 +=1
